5d4c1891f0e6a58b1219708f17809d67cd2d202e
jaymithani1998 <114779988+jaymithani1998@users.noreply.github.com>
2022-12-01 22:04:48 -0500

    Auth code rebase

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,11 @@
+//File to aunthenticate details entered by user
+
 const router = require("express").Router();
 const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
-const Joi = require("joi");
+const JOI = require("joi");
 
+//Create Route for post
 router.post("/", async (req, res) => {
 	try {
 		const { error } = validate(req.body);
@@ -13,24 +16,25 @@ router.post("/", async (req, res) => {
 		if (!user)
 			return res.status(401).send({ message: "Invalid Email or Password" });
 
-		const validPassword = await bcrypt.compare(
+		const isValidPassword = await bcrypt.compare(
 			req.body.password,
 			user.password
 		);
-		if (!validPassword)
-			return res.status(401).send({ message: "Invalid Email or Password" });
+		if (!isValidPassword)
+			return res.status(401).send({ message: "Invalid Email or Password. Please Enter correct details." });
 
 		const token = user.generateAuthToken();
-		res.status(200).send({ data: token, message: "logged in successfully" });
+		res.status(200).send({ data: token, message: "Logged in successfully" });
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
 	}
 });
 
+//Validate data
 const validate = (data) => {
-	const schema = Joi.object({
-		email: Joi.string().email().required().label("Email"),
-		password: Joi.string().required().label("Password"),
+	const schema = JOI.object({
+		email: JOI.string().email().required().label("Email"),
+		password: JOI.string().required().label("Password"),
 	});
 	return schema.validate(data);
 };
